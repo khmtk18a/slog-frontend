30124ca077632126bb572b8844e3b742e259f3a2
ging-dev <thanh1101dev@gmail.com>
2023-08-07 13:48:26 +0700

    chore: cleanup

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -15,38 +15,16 @@ export const useAuthStore = defineStore('auth', {
     // state: () => ({ count: 0 })
     actions: {
         checkLoginStatus() {
-            FB.getLoginStatus((response) => {   // Called after the JS SDK has been initialized.
-                if (response.status == "connected") {
-
-                    this.authResponse = response.authResponse
-                    this.fetchUserInfor()
-                }
-
-            });
         },
         fetchUserInfor() {
             this.isLogined = true
             this.authOpen = false
-            FB.api('/me', { fields: 'name,email,picture' }, (response) => {
-                this.userInfo = { ...response }
-
-            })
         }
         ,
         loginByFacebook() {
-            FB.login(response => {
-                if (response.authResponse) {
-                    this.fetchUserInfor()
-                } else {
-                    console.log('User cancelled login or did not fully authorize.')
-                }
-            })
+
         },
         logoutByFacebook() {
-            FB.logout(response => {
-                this.isLogined = false
-                console.log(response)
-            })
         }
     },
 })
